0d6d3a2235d249246e23a1e543a274e471b99576
Kitupala <kimmo.tuupanen@gmail.com>
2024-10-08 13:14:31 +0300

    Animate TechList

--- src/slices/TechList/index.tsx
+++ src/slices/TechList/index.tsx
@@ -1,18 +1,60 @@
+"use client";
+
 import Bounded from "@/app/components/Bounded";
 import Heading from "@/app/components/Heading";
+import { useGSAP } from "@gsap/react";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
-import React from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import React, { useRef } from "react";
 import { MdCircle } from "react-icons/md";
-import { div } from "three/webgpu";
 
 export type TechListProps = SliceComponentProps<Content.TechListSlice>;
 
 const TechList = ({ slice }: TechListProps): JSX.Element => {
+  const container = useRef(null);
+  gsap.registerPlugin(useGSAP, ScrollTrigger);
+
+  useGSAP(
+    () => {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          // markers: true,
+          trigger: container.current,
+          start: "top bottom",
+          end: "bottom top",
+          scrub: 4,
+        },
+      });
+
+      tl.fromTo(
+        ".tech-row",
+        {
+          x: (index) => {
+            return index % 2 === 0
+              ? gsap.utils.random(200, 100)
+              : gsap.utils.random(-200, -100);
+          },
+        },
+        {
+          x: (index) => {
+            return index % 2 === 0
+              ? gsap.utils.random(-200, -100)
+              : gsap.utils.random(200, 100);
+          },
+          ease: "power1.inOut",
+        },
+      );
+    },
+    { scope: container },
+  );
+
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      ref={container}
     >
       <Bounded as="div">
         <Heading size="lg" as="h2">
