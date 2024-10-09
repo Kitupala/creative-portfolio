97e10f43480d98868ab1b4ae5a3bb972d002d813
Kitupala <kimmo.tuupanen@gmail.com>
2024-10-09 18:15:50 +0300

    Add background noise texture and gradient

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Titillium_Web } from "next/font/google";
 import "./globals.css";
 import Header from "@/app/components/Header";
+import Footer from "@/app/components/Footer";
+import clsx from "clsx";
 
 const titillium = Titillium_Web({
   subsets: ["latin"],
@@ -22,10 +24,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="bg-slate-900 text-slate-100 antialiased">
-      <body className={`${titillium.className}`}>
+      {/* <body className={`${titillium.className}`}> */}
+      <body className={clsx(titillium.className, "relative min-h-screen")}>
         <Header />
         {children}
-        <div className="h-[100vh]" />
+        <Footer />
+        <div className="background-gradient absolute inset-0 -z-50 max-h-screen"></div>
+        <div className="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-10 mix-blend-soft-light"></div>
       </body>
     </html>
   );
