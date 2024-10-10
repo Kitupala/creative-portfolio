9457cfa1a9070792c88eb664b58e07e6d6ad5e3c
Kitupala <kimmo.tuupanen@gmail.com>
2024-10-10 14:22:32 +0300

    Fix urlPrefix

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -31,7 +31,7 @@ export default function ContentList({
   const [hovering, setHovering] = useState(false);
   const lastMousePos = useRef({ x: 0, y: 0 });
 
-  const urlPrefix = contentType === "Blog" ? "/blog" : "/project";
+  const urlPrefix = contentType === "Blog" ? "/blog" : "/projects";
 
   useGSAP(() => {
     itemsRef.current.forEach((item) => {
